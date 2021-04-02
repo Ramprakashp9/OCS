c55fd529e78f6e64a15adc986978110b0fe72192
Ramprakashp9 <81757506+Ramprakashp9@users.noreply.github.com>
2021-04-02 17:26:01 +0530

    Update installocs.sh

--- installocs.sh
+++ installocs.sh
@@ -7,7 +7,7 @@
  #to get configuration files  
 wget https://raw.githubusercontent.com/Ramprakashp9/OCS/main/CaCert.pem
 
-wget  https://github.com/Ramprakashp9/OCS/raw/main/Ocsinventory-Unix-Agent-2.4.2%20(1).tar.gz
+wget  https://github.com/Ramprakashp9/OCS/raw/main/Ocsinventory-Unix-Agent-2.4.2.tar.gz
  
 wget https://raw.githubusercontent.com/Ramprakashp9/OCS/main/ocsinventory-agent.cfg
 
